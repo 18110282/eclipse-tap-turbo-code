68f42534a714b1374bd156854670d27600f00bb4
Hồ Hiếu <54926587+18110282@users.noreply.github.com>
2025-01-21 13:58:20 +0700

    Update pro-clicks.js

--- pro-clicks.js
+++ pro-clicks.js
@@ -1,7 +1,7 @@
 const centerX = window.innerWidth * 35 / 100;
 const centerY = window.innerHeight / 2;
-let xMin = centerX - (centerX * 20 / 100),
-    xMax = centerX + (centerX * 20 / 100),
+let xMin = centerX - (centerX * 25 / 100),
+    xMax = centerX + (centerX * 25 / 100),
     yMin = centerY - (centerY * 15 / 100),
     yMax = centerY + (centerY * 15 / 100);
 	
